2770e475c3421670f04652f6ea21b1a337bcc9f0
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-01-16 12:53:04 +0500

    setup configure

--- src/app.js
+++ src/app.js
@@ -15,13 +15,18 @@ app.use(
 
 // MIDDLEWARES
 
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
+app.use(express.json({limit: "16kb",}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// ROUTES IMPORT ;
+
+import userRoutes from './routes/user.routes.js';
+
+// ROUTES DICLERATION ;
+
+app.use('/api/v1/users',userRoutes);
 
 export { app };
