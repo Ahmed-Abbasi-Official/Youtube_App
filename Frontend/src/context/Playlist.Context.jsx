964bbba9c5101e0e37ffbf4991549b9d71b49ac8
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-02-06 22:42:36 +0500

    new updates

--- Frontend/src/context/Playlist.Context.jsx
+++ Frontend/src/context/Playlist.Context.jsx
@@ -5,7 +5,7 @@ import axios from "axios"
 import { createContext, useContext, useMemo, useCallback } from "react"
 
 const PlaylistContext = createContext(null)
-const BASE_URL = "http://localhost:4000/api/v1/playlist"
+const BASE_URL = "https://play-lgud.onrender.com/api/v1/playlist"
 
 export const PlaylistProvider = ({ children }) => {
   const queryClient = useQueryClient()
